2442709732a9fb63a0034f2686fa2cc2f7e032f3
ChihTeng Ma <chihtengma416@gmail.com>
2023-06-06 15:55:14 -0400

    Added About Us

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,19 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
-
- const Navbar = () => {
-    return (
-        <nav>
-              <Link to='/'>Home</Link>
-              <Link to='/'>About</Link>
-              <Link to='/cart'>Cart</Link>
-              <div>
-                <Link to='/login'>Login</Link>
-              </div>
-        </nav>
-    )
-} 
+const Navbar = () => {
+   return (
+      <nav>
+         <Link to="/">Home</Link>
+         <Link to="/about">About</Link>
+         <Link to="/cart">Cart</Link>
+         <div>
+            <Link to="/login">Login</Link>
+         </div>
+      </nav>
+   );
+};
 
 export default Navbar;
