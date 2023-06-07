33c1dd2a55081d44112f6badcb39aff708243cc7
Yimin Yang <yvonne.yangh@gmail.com>
2023-06-06 20:37:34 -0500

    update add to cart feature
    update add to cart feature

--- src/store/cart.js
+++ src/store/cart.js
@@ -19,7 +19,28 @@ export const fetchCart = createAsyncThunk("fetchCart", async()=>{
   }
 })
 
+export const addToCart = createAsyncThunk('addToCart', async({product,quantity}) =>{
+  try{
+    const token = window.localStorage.getItem('token');
+    const obj = {product,quantity}
+    const response = await axios.post('/api/orders/cart',{product,quantity},{
+      headers: {
+        authorization: token
+      }
+    });
+    return response.data;
+  }catch(err){
+    console.log(err);
+  }
+
+})
+
 export const updateCart = createAsyncThunk('updateCart', async(request) =>{
+  const response = await axios.put('/api/orders/cart',request);
+  return response.data;
+})
+
+export const removeCart = createAsyncThunk('removeCart', async(request) =>{
   const response = await axios.put('/api/orders/cart',request);
   return response.data;
 })
@@ -30,6 +51,9 @@ const cartSlice = createSlice({
   reducers: {},
   extraReducers: (builder)=>{
     builder.addCase(fetchCart.fulfilled, (state, action)=>{
+      return action.payload;
+    })
+    builder.addCase(addToCart.fulfilled, (state, action)=>{
       return action.payload;
     })
    
