32b436c9aa092979d0f5a8af84af3772602b73ff
Jerry Chen <jerryc830@gmail.com>
2023-06-07 16:12:31 -0400

    login cat pic

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -56,43 +56,61 @@ const Login = () => {
   return (
     <div className="container py-5 h-100">
       <div className="row d-flex justify-content-center align-items-center h-100">
-        <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-          <div
-            className="card shadow-2-strong"
-            style={{ borderRadius: "1rem" }}
-          >
-            <div className="card-body p-5 text-center">
-              <h2 className="mb-5">Login</h2>
-              <form onSubmit={login}>
-                <div className="form-outline mb-4">
-                  <input
-                    placeholder="Username"
-                    value={credentials.username}
-                    name="username"
-                    onChange={onChange}
-                    className="form-control form-control-lg"
-                  />
+        <div className="col col-xl-10">
+          <div className="card" style={{ borderRadius: "1rem" }}>
+            <div className="row g-0">
+              <div className="col-md-6 col-lg-5 d-none d-md-block">
+                <img
+                  src="https://img.freepik.com/free-photo/isolated-closeup-shot-gray-cat-looking-into-camera_181624-60601.jpg?w=826&t=st=1686164569~exp=1686165169~hmac=5388beeda2e1479f832ab1556f1e3283a1fd995c6f96e7f96d2548c7753dc34e"
+                  alt="login form"
+                  className="img-fluid"
+                  style={{ borderRadius: "1rem 0 0 1rem" }}
+                />
+              </div>
+              <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                <div className="card-body p-4 p-lg-5 text-black">
+                  <h2 className="mb-5 d-flex justify-content-center align-items-center">Login</h2>
+                  <form onSubmit={login}>
+                    <div className="form-outline mb-4">
+                      <input
+                        placeholder="Username"
+                        value={credentials.username}
+                        name="username"
+                        onChange={onChange}
+                        className="form-control form-control-lg"
+                      />
+                    </div>
+                    <div className="form-outline mb-4">
+                      <input
+                        placeholder="Password"
+                        type="password"
+                        name="password"
+                        value={credentials.password}
+                        onChange={onChange}
+                        className="form-control form-control-lg"
+                      />
+                    </div>
+                    <div className="mb-2 d-flex justify-content-center align-items-center">
+                      <button
+                        disabled={invalidCredentials}
+                        className="btn btn-primary btn-lg btn-block"
+                      >
+                        Login
+                      </button>
+                    </div>
+                    <div className="d-flex justify-content-center align-items-center">
+                      <button
+                        onClick={register}
+                        disabled={invalidCredentials}
+                        className="btn btn-primary btn-lg btn-block"
+                      >
+                        Register
+                      </button>
+                    </div>
+                  </form>
+                  {loginError && <div>{loginError}</div>}
                 </div>
-                <div className="form-outline mb-4">
-                  <input
-                    placeholder="Password"
-                    type="password"
-                    name="password"
-                    value={credentials.password}
-                    onChange={onChange}
-                    className="form-control form-control-lg"
-                  />
-                </div>
-                <div className="mb-2">
-                  <button disabled={invalidCredentials} className="btn btn-primary btn-lg btn-block">Login</button>
-                </div>
-                <div>
-                  <button onClick={register} disabled={invalidCredentials} className="btn btn-primary btn-lg btn-block">
-                    Register
-                  </button>
-                </div>
-              </form>
-              {loginError && <div>{loginError}</div>}
+              </div>
             </div>
           </div>
         </div>
