8a83bb2639790d2d28727647c9a3a2f349b6df2d
Jerry Chen <jerryc830@gmail.com>
2023-06-07 19:49:34 -0400

    seperate route for register
    we are missing the logout button with the logged in user showing

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,56 +1,72 @@
 import React, { useState } from "react";
-import { useSelector, useDispatch } from 'react-redux';
-import { logout } from '../store';
-import { Link } from 'react-router-dom';
-import { addToCart,removeFromCart } from "../store";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../store";
+import { Link } from "react-router-dom";
+import { addToCart, removeFromCart } from "../store";
 
-const Cart = ()=> {
-  const { cart } = useSelector(state => state);
+const Cart = () => {
+  const { cart } = useSelector((state) => state);
   const dispatch = useDispatch();
-  const [items,setItems] = useState([]);
+  const [items, setItems] = useState([]);
 
   React.useEffect(() => {
     const list = [...cart.lineItems];
-    if(list){
-      list.sort(function(a,b){
-      if (a.product.name < b.product.name) {
-        return -1;
-      }
-      if (a.product.name > b.product.name) {
-        return 1;
-      }
-      return 0;
-    })
-    console.log(list,'line24');
-  }
-    
+    if (list) {
+      list.sort(function (a, b) {
+        if (a.product.name < b.product.name) {
+          return -1;
+        }
+        if (a.product.name > b.product.name) {
+          return 1;
+        }
+        return 0;
+      });
+      console.log(list, "line24");
+    }
+
     setItems(list);
-  },[cart])
-  
-  console.log(items);
+  }, [cart]);
+
   return (
-    <div>
-      <h1>Cart</h1>
-      <div>
-        {items.map((item) => {
-          return(
-          <div key={item.id}>
-            <p>name: {item.product.name}</p>
-            <p>quantity: {item.quantity}</p>
-            <button onClick={()=>dispatch(removeFromCart({product:item.product,quantityToRemove:1}))}>-</button>
-            <input type="number" value={item.quantity} min="1" max="5"/>
-            <button onClick={()=>dispatch(addToCart({product:item.product,quantity:1}))}>+</button>
-            <hr/>
-          </div>
-          )
-        })}
-        
+    <div className="container py-5 h-100">
+      <div className="row d-flex justify-content-center align-items-center h-100">
+        <div className="col-12">
+        <h1>Cart</h1>
+        <div>
+          {items.map((item) => {
+            return (
+              <div key={item.id}>
+                <img src={item.product.images[0]} />
+                <p>name: {item.product.name}</p>
+                <p>quantity: {item.quantity}</p>
+                <button
+                  onClick={() =>
+                    dispatch(
+                      removeFromCart({
+                        product: item.product,
+                        quantityToRemove: 1,
+                      })
+                    )
+                  }
+                >
+                  -
+                </button>
+                <input type="number" value={item.quantity} min="1" max="5" />
+                <button
+                  onClick={() =>
+                    dispatch(addToCart({ product: item.product, quantity: 1 }))
+                  }
+                >
+                  +
+                </button>
+                <hr />
+              </div>
+            );
+          })}
+          <button>Checkout</button>
+        </div>
+        </div>
       </div>
-      <>
-        {/*
-          JSON.stringify(cart, null, 2)
-      */}
-      </>
     </div>
   );
 };
