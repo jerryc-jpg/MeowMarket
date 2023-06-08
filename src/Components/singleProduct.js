015df8be275eb8aa1ba71adc4dea07abd4e434fa
ChihTeng Ma <chihtengma416@gmail.com>
2023-06-08 17:26:58 -0400

    Single cat page style

--- src/Components/singleProduct.js
+++ src/Components/singleProduct.js
@@ -6,105 +6,113 @@ import SingleProductAdmin from "./SingleProductAdmin";
 import { deleteProduct } from "../store";
 
 const SingleProduct = () => {
-  const { products, auth } = useSelector((state) => state);
-  const dispatch = useDispatch();
-  const { id } = useParams();
-  const navigate = useNavigate();
-  const [quantity, setQuantity] = useState(1);
-  const [oneProd, setOneProd] = useState({});
-  const isAdmin = auth.isAdmin;
-  React.useEffect(() => {
-    const foundProd = products.find((product) => product.id === id);
-    if (!foundProd) {
+   const { products, auth } = useSelector((state) => state);
+   const dispatch = useDispatch();
+   const { id } = useParams();
+   const navigate = useNavigate();
+   const [quantity, setQuantity] = useState(1);
+   const [oneProd, setOneProd] = useState({});
+   const isAdmin = auth.isAdmin;
+   React.useEffect(() => {
+      const foundProd = products.find((product) => product.id === id);
+      if (!foundProd) {
+         navigate("/");
+      } else {
+         setOneProd(foundProd);
+      }
+   }, [products, id]);
+
+   const decrementQ = (value) => {
+      if (value > 1) {
+         setQuantity(value - 1);
+      }
+   };
+
+   const incrementQ = (value) => {
+      if (value < 5) {
+         setQuantity(value + 1);
+      }
+   };
+
+   const handleQuantityChange = (ev) => {
+      const value = Number(ev.target.value);
+      if (value >= 1 && value <= 5) {
+         setQuantity(value);
+      }
+   };
+
+   const handleDelete = () => {
+      dispatch(deleteProduct(oneProd));
       navigate("/");
-    } else {
-      setOneProd(foundProd);
-    }
-  }, [products, id]);
+   };
 
-  const decrementQ = (value) => {
-    if (value > 1) {
-      setQuantity(value - 1);
-    }
-  };
-
-  const incrementQ = (value) => {
-    if (value < 5) {
-      setQuantity(value + 1);
-    }
-  };
-
-  const handleQuantityChange = (ev) => {
-    const value = Number(ev.target.value);
-    if (value >= 1 && value <= 5) {
-      setQuantity(value);
-    }
-  };
-
-  const handleDelete = () => {
-    dispatch(deleteProduct(oneProd));
-    navigate("/");
-    };
-
-
-  if (oneProd.productType === "cat") {
-    return (
-      <>
-        <p>Cat</p>
-        <p>Name:{oneProd.name}</p>
-        <p>Breed:{oneProd.breed}</p>
-        <p>Age:{oneProd.age}</p>
-        <p>Description:{oneProd.description}</p>
-        <p>Price:{oneProd.price}</p>
-        <img src={oneProd.images[0]} />
-        {isAdmin ? (
-          <>
-            {" "}
-            <Link to={`/admin/${oneProd.id}`}>
-              <button>Edit</button>
-            </Link>
+   if (oneProd.productType === "cat") {
+      return (
+         <div className="container vertical-center">
+            <div className="row justify-content-center">
+               <div className="col-md-6">
+                  <div className="card">
+                     <img src={oneProd.images[0]} alt="Cat Image" className="card-img-top img-fluid" />
+                  </div>
+               </div>
+               <div className="col-md-6">
+                  <h2>Cat Details</h2>
+                  <p className="cat-detail">
+                     <strong>Name:</strong> {oneProd.name}
+                  </p>
+                  <p className="cat-detail">
+                     <strong>Breed:</strong> {oneProd.breed}
+                  </p>
+                  <p className="cat-detail">
+                     <strong>Age:</strong> {oneProd.age}
+                  </p>
+                  <p className="cat-detail">
+                     <strong>Description:</strong>
+                  </p>
+                  <p class="description">{oneProd.description}</p>
+                  <p className="cat-detail">
+                     <strong>Price:</strong> ${oneProd.price}
+                  </p>
+                  {isAdmin ? (
+                     <div className="mt-3">
+                        <Link to={`/admin/${oneProd.id}`} className="btn btn-primary me-2">
+                           Edit
+                        </Link>
+                        <button onClick={handleDelete} className="btn btn-danger">
+                           Delete
+                        </button>
+                     </div>
+                  ) : (
+                     <button
+                        className="btn btn-success mt-3"
+                        onClick={() => {
+                           dispatch(addToCart({ product: oneProd, quantity }));
+                        }}>
+                        Add Cart
+                     </button>
+                  )}
+               </div>
+            </div>
+         </div>
+      );
+   } else {
+      return (
+         <>
+            <h1>accessory</h1>
+            <h1>{oneProd.name}</h1>
+            <img src={oneProd.images} />
+            <button onClick={() => decrementQ(quantity)}>-</button>
+            <input type="number" value={quantity} min="1" max="5" onChange={handleQuantityChange} />
+            <button onClick={() => incrementQ(quantity)}>+</button>
             <button
-              onClick={handleDelete}
-            >
-              Delete
+               onClick={() => {
+                  dispatch(addToCart({ product: oneProd, quantity }));
+               }}>
+               Add Cart
             </button>
-          </>
-        ) : (
-          <button
-            onClick={() => {
-              dispatch(addToCart({ product: oneProd, quantity }));
-            }}
-          >
-            Add Cart
-          </button>
-        )}
-      </>
-    );
-  } else {
-    return (
-      <>
-        <h1>accessory</h1>
-        <h1>{oneProd.name}</h1>
-        <img src={oneProd.images} />
-        <button onClick={() => decrementQ(quantity)}>-</button>
-        <input
-          type="number"
-          value={quantity}
-          min="1"
-          max="5"
-          onChange={handleQuantityChange}
-        />
-        <button onClick={() => incrementQ(quantity)}>+</button>
-        <button
-          onClick={() => {
-            dispatch(addToCart({ product: oneProd, quantity }));
-          }}
-        >
-          Add Cart
-        </button>
-      </>
-    );
-  }
+         </>
+      );
+   }
 };
 
 export default SingleProduct;
