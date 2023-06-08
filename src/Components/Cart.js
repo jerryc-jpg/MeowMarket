cf2132c5139f8bb1e6f7bbb09085f9199e358f7c
Yimin Yang <yvonne.yangh@gmail.com>
2023-06-08 16:21:09 -0500

    refine add to cart

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -8,6 +8,7 @@ const Cart = ()=> {
   const { cart } = useSelector(state => state);
   const dispatch = useDispatch();
   const [items,setItems] = useState([]);
+<<<<<<< Updated upstream
 
   React.useEffect(() => {
       setItems(cart.lineItems);
@@ -27,6 +28,111 @@ const Cart = ()=> {
             <input type="number" Value={item.quantity} min="1" max="5"/>
             <button onClick={()=>dispatch(addToCart({product:item.product,quantity:1}))}>+</button>
             <hr/>
+=======
+  const [totalPrice,setTotalPrice] = useState(0);
+  const [totalQ,setTotalQ] = useState(0);
+  const navigate = useNavigate();
+
+  const handleCheckout = () => {
+    dispatch(checkoutCart());
+    navigate('/cart/checkout');
+    
+  }
+
+  React.useEffect(() => {
+
+    let list = [...cart.lineItems];
+
+    if (list) {
+      list.sort(function (a, b) {
+        if (a.product.name < b.product.name) {
+          return -1;
+        }
+        if (a.product.name > b.product.name) {
+          return 1;
+        }
+        return 0;
+      });
+      console.log(list, "line24");
+    }
+
+    setItems(list);
+    
+    const sumPrice = list.reduce((acc,curr)=>{
+      acc = acc + curr.product.price * curr.quantity;
+      return acc;
+    },0)
+
+    const sumQ = list.reduce((acc,curr)=>{
+      acc = acc+curr.quantity;
+      return acc;
+    },0)
+
+    setTotalPrice(sumPrice);
+
+    setTotalQ(sumQ);
+
+  }, [cart]);
+  console.log(items,"line46");
+  return (
+    <div className="container py-5 h-100">
+      <div className="row d-flex justify-content-center align-items-center h-100">
+        <div className="col-12">
+        <h1>Cart</h1>
+        <div>
+          {items.map((item) => {
+            return (
+              <div key={item.id}>
+                <img src={item.product.images[0]} />
+                <p>name: {item.product.name}</p>
+                <p>quantity: {item.quantity}</p>
+                {item.product.productType!=='cat'?
+                  <>
+                    <button
+                    onClick={() =>
+                      dispatch(
+                        removeFromCart({
+                          product: item.product,
+                          quantityToRemove: 1,
+                        })
+                      )
+                    }
+                    >
+                      -
+                    </button>
+                    <input type="number" value={item.quantity} min="1" max="5" />
+                    <button
+                      onClick={() =>
+                        dispatch(addToCart({ product: item.product, quantity: 1 }))
+                      }
+                    >
+                      +
+                    </button>
+                  </>:null
+                }
+               
+                <button onClick={() =>
+                      dispatch(
+                        removeFromCart({
+                          product: item.product,
+                          quantityToRemove: item.quantity,
+                        }))}
+                      >remove from cart</button>
+                <hr />
+              </div>
+            );
+          })}
+          <div>
+            <div>
+              <span>QUANTITY TOTAL: </span>
+              <span>{totalQ}</span>
+            </div>
+            <div>
+              <span>ORDER TOTAL: </span>
+              <span>{totalPrice}</span>
+            </div>
+            <button onClick={()=>handleCheckout()}>Checkout</button>
+>>>>>>> Stashed changes
           </div>
           )
         })}
--- src/Components/singleProduct.js
+++ src/Components/singleProduct.js
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import { addToCart } from "../store";
 
 const SingleProduct = () => {
+<<<<<<< Updated upstream
     const {products} = useSelector(state => state);
     const dispatch = useDispatch();
     const [quantity,setQuantity] = useState(1);
@@ -12,6 +13,26 @@ const SingleProduct = () => {
         if(quantity>1){
             setQuantity(quantity - 1);
         }
+=======
+  const { products, auth, cart } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [quantity, setQuantity] = useState(1);
+  const [oneProd, setOneProd] = useState({});
+  const isAdmin = auth.isAdmin;
+
+  const isActiveAdd = (inputId) =>{
+    const activeAdd = cart.lineItems.reduce((acc,curr) =>{return acc && curr.productId!== inputId }, true)
+    return activeAdd;
+ }
+  React.useEffect(() => {
+    const foundProd = products.find((product) => product.id === id);
+    if (!foundProd) {
+      navigate("/");
+    } else {
+      setOneProd(foundProd);
+>>>>>>> Stashed changes
     }
 
     const incrementQ = () => {
@@ -26,8 +47,68 @@ const SingleProduct = () => {
     if (!oneProd) {
         return null;
     }
+<<<<<<< Updated upstream
     return(
     <>
+=======
+  };
+
+  const handleQuantityChange = (ev) => {
+    const value = Number(ev.target.value);
+    if (value >= 1 && value <= 5) {
+      setQuantity(value);
+    }
+  };
+
+  const handleDelete = () => {
+    dispatch(deleteProduct(oneProd));
+    navigate("/");
+    };
+
+
+  if (oneProd.productType === "cat") {
+    return (
+      <>
+        <p>Cat</p>
+        <p>Name:{oneProd.name}</p>
+        <p>Breed:{oneProd.breed}</p>
+        <p>Age:{oneProd.age}</p>
+        <p>Description:{oneProd.description}</p>
+        <p>Price:{oneProd.price}</p>
+        <img src={oneProd.images[0]} />
+        {isAdmin ? (
+          <>
+            {" "}
+            <Link to={`/admin/${oneProd.id}`}>
+              <button>Edit</button>
+            </Link>
+            <button
+              onClick={handleDelete}
+            >
+              Delete
+            </button>
+          </>
+        ) : (
+          <button
+            onClick={() => {if(isActiveAdd(id)){
+                dispatch(addToCart({ product: oneProd, quantity }))
+            }else{
+                window.alert("Maximum quantity reached!");
+                console.log("Maximum quantity reached!");
+            }
+              ;
+            }}
+          >
+            TAKE ME HOME
+          </button>
+        )}
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1>accessory</h1>
+>>>>>>> Stashed changes
         <h1>{oneProd.name}</h1>
         <button onClick={decrementQ}>-</button>
         <input type="number" defaultValue={quantity} min="1" max="5"/>
