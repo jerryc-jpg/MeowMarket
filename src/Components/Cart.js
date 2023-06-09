2f9c1bc6d9ae0e29fabdbe639d07065e96ac0129
Jerry Chen <jerryc830@gmail.com>
2023-06-08 23:26:24 -0400

    cart styling

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -52,81 +52,76 @@ const Cart = () => {
   }, [cart]);
  
   return (
-    <div className="container py-5 h-100">
-      <div className="row d-flex justify-content-center align-items-center h-100">
+    <div className="container py-5">
+      <div className="row">
         <div className="col-12">
-        <h1>Cart</h1>
-        <div>
-          {items.map((item) => {
-            return (
-              <div key={item.id}>
-                <img src={item.product.images[0]} />
-                <p>name: {item.product.name}</p>
-                <p>quantity: {item.quantity}</p>
-                {
-                item.product.productType!=='cat'?
-                  <>
-                    <button
-                      onClick={() =>{
-                          dispatch(
-                            removeFromCart({
-                              product: item.product,
-                              quantityToRemove: 1,
-                            })
-                          )
-                        }  
-                      }
-                    >
-                      -
-                    </button>
-                    <input type="number" value={item.quantity} min="1" max="5" />
-                    <button
-                      onClick={() =>{
-                          dispatch(updateProductQuantity({product:item.product,quantity:-1}));
-                          dispatch(addToCart({ product: item.product, quantity: 1 }))
-                        }
-                      }
-                    >
-                      +
-                    </button>
-                  </>:null
-                }
-               
-                <button 
-                  onClick={() =>{
-                    dispatch(updateProductQuantity({
-                      product:item.product,
-                      quantity:item.quantity*-1}));
-                    dispatch(
-                      removeFromCart({
-                        product: item.product,
-                        quantityToRemove: item.quantity,
-                      }))
-                    }
-                  }
-                >
-                  remove from cart
-                </button>
-                <hr />
+          <h1 className="mb-4">Shopping Cart</h1>
+          {items.map((item) => (
+            <div className="mb-4" key={item.id}>
+              <div className="row align-items-center">
+                <div className="col-auto">
+                  <img src={item.product.images[0]} alt={item.product.name} className="rounded img-fluid" width={"250px"}/>
+                </div>
+                <div className="col">
+                  <p className="mb-0">Name: {item.product.name}</p>
+                  <p className="mb-0">Quantity: {item.quantity}</p>
+                  {item.product.productType !== "cat" && (
+                    <>
+                      <button
+                        className="btn btn-sm btn-outline-primary me-1"
+                        onClick={() => {
+                          dispatch(removeFromCart({ product: item.product, quantityToRemove: 1 }));
+                        }}
+                      >
+                        -
+                      </button>
+                      <input
+                        type="number"
+                        value={item.quantity}
+                        min="1"
+                        max="5"
+                        className="form-control d-inline-block w-auto me-1"
+                      />
+                      <button
+                        className="btn btn-sm btn-outline-primary me-1"
+                        onClick={() => {
+                          dispatch(updateProductQuantity({ product: item.product, quantity: -1 }));
+                          dispatch(addToCart({ product: item.product, quantity: 1 }));
+                        }}
+                      >
+                        +
+                      </button>
+                    </>
+                  )}
+                  <button
+                    className="btn btn-sm btn-danger"
+                    onClick={() => {
+                      dispatch(updateProductQuantity({ product: item.product, quantity: item.quantity * -1 }));
+                      dispatch(removeFromCart({ product: item.product, quantityToRemove: item.quantity }));
+                    }}
+                  >
+                    Remove from Cart
+                  </button>
+                </div>
               </div>
-            );
-          })}
+              <hr />
+            </div>
+          ))}
+          <div className="d-flex justify-content-end mb-4">
+            <div className="cart-summary">
+              <p>
+                <span>Quantity Total: </span>
+                <span>{totalQ}</span>
+              </p>
+              <p>
+                <span>Order Total: </span>
+                <span>${totalPrice}</span>
+              </p>
+              <button className="btn btn-primary" onClick={() => handleCheckout()}>
+                Checkout
+              </button>
+            </div>
           </div>
-          <div>
-
-
-            <p>
-              <span>QUANTITY TOTAL: </span>
-              <span>{totalQ}</span>
-            </p>
-            <p>
-              <span>ORDER TOTAL: </span>
-              <span>{totalPrice}</span>
-            </p>
-            <button onClick={()=>handleCheckout()}>Checkout</button>
-          </div>
-          
-        
         </div>
       </div>
     </div>
