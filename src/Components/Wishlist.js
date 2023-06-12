f70d63b6b3d5e2bf88fa7d86ecac0a88c76b84af
Jerry Chen <jerryc830@gmail.com>
2023-06-12 16:28:27 -0400

    wishlist (need some work still)

---
+++ src/Components/Wishlist.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
+
+const Wishlist = () => {
+    return (
+        <div>
+            <h1>Wishlist</h1>
+        </div>
+    )
+}
+
+export default Wishlist;
