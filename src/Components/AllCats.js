d691b749a250b20f66a2375a5c793d79d708c37d
ChihTeng Ma <chihtengma416@gmail.com>
2023-06-13 15:03:40 -0400

    Update AllCats.js

--- src/Components/AllCats.js
+++ src/Components/AllCats.js
@@ -5,151 +5,122 @@ import { useDispatch } from "react-redux";
 import { addToCart, updateProductQuantity, addToWishlist } from "../store";
 
 const AllCats = ({ filter }) => {
-  const dispatch = useDispatch();
-  const { products, cart } = useSelector((state) => state);
-  const [allCats, setAllCats] = useState([]);
+   const dispatch = useDispatch();
+   const { products, cart } = useSelector((state) => state);
+   const [allCats, setAllCats] = useState([]);
 
-  //new////
-  const [currentPageCats, setCurrentPageCats] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 9;
-  
-  const hanelPreviousPage = () =>{
-   setCurrentPage((Page) => Page - 1);
-   handelUpdatePageList(allCats,currentPage);
-  };
-  const handelNextPage = () => {
-   setCurrentPage((Page) => Page + 1);
-   handelUpdatePageList(allCats,currentPage);
-  };
-  ///////
+   //new////
+   const [currentPage, setCurrentPage] = useState(1);
+   const itemsPerPage = 9;
 
-  // didn't use this sortProduct
-  // const sortProduct = (list) => {
-  //   if (list) {
-  //     list.sort(function (a, b) {
-  //       if (a.quantity > b.quantity) {
-  //         return -1;
-  //       }
-  //       if (a.quantity < b.quantity) {
-  //         return 1;
-  //       }
-  //       return 0;
-  //     });
-  //   }
-  // };
+   const handlePreviousPage = () => {
+      setCurrentPage((Page) => Page - 1);
+   };
+   const handleNextPage = () => {
+      setCurrentPage((Page) => Page + 1);
+   };
+   ///////
 
-  const addToWishlistHandler = (product) => {
-    dispatch(addToWishlist(product));
-    console.log("add to wishlist");
-  };
-  
+   // didn't use this sortProduct
+   // const sortProduct = (list) => {
+   //   if (list) {
+   //     list.sort(function (a, b) {
+   //       if (a.quantity > b.quantity) {
+   //         return -1;
+   //       }
+   //       if (a.quantity < b.quantity) {
+   //         return 1;
+   //       }
+   //       return 0;
+   //     });
+   //   }
+   // };
 
-  
-  
-  React.useEffect(() => {
-    let cats = products.filter((product) => product.productType === "cat");
-    //new///
-    //
-    //console.log(cats,'cats');
-    let catlist = [...cats];
-    if (catlist) {
-      //console.log('works');
-      catlist.sort(function (a, b) {
-        if (a.quantity > b.quantity) {
-          return -1;
-        }
-        if (a.quantity < b.quantity) {
-          return 1;
-        }
-        return 0;
-      });
-    }
-    setAllCats(catlist);
-  }, [products, cart]);
+   const addToWishlistHandler = (product) => {
+      dispatch(addToWishlist(product));
+      console.log("add to wishlist");
+   };
 
+   React.useEffect(() => {
+      let cats = products.filter((product) => product.productType === "cat");
 
+      let catlist = [...cats];
+      if (catlist) {
+         catlist.sort(function (a, b) {
+            if (a.quantity > b.quantity) {
+               return -1;
+            }
+            if (a.quantity < b.quantity) {
+               return 1;
+            }
+            return 0;
+         });
+      }
+      setAllCats(catlist);
+   }, [products, cart]);
 
+   const AllfilteredCats = allCats.filter((cat) => cat.name.toLowerCase().includes(filter.toLowerCase()));
 
+   const indexOfLastCat = currentPage + itemsPerPage;
+   const indexOfFirstCat = indexOfLastCat - itemsPerPage;
+   const currentCats = AllfilteredCats.slice(indexOfFirstCat, indexOfLastCat);
 
-  //console.log('allCats:',allCats);
-  const AllfilteredCats = allCats.filter((cat) =>
-    cat.name.toLowerCase().includes(filter.toLowerCase())
-  );
-  let filteredCats = [...AllfilteredCats].slice((currentPage-1)*itemsPerPage,currentPage*itemsPerPage);
-  let totalPages = Math.ceil(allCats.length/6);
+   const totalPages = Math.ceil(AllfilteredCats.length / itemsPerPage);
 
-  
-  
- 
-  //new
-  console.log("AllCats.js totalPages:",totalPages);
-  console.log("AllCats.js allCats:", allCats)
-  console.log("AllCats.js current page cats:",currentPageCats);
-  
-
-  return (
-
-    <div className="container text-center">
-      <div className="row">
-        {filteredCats.map((cat) => (
-          <div key={cat.id} className="col-lg-4 col-md-6 col-sm-12 mb-4">
-            <div className="card h-100">
-              <div className="ratio ratio-4x3">
-                <img
-                  className="card-img-top img-fluid"
-                  src={cat.images[0]}
-                  alt={cat.name}
-                />
-              </div>
-              <div className="card-body">
-                <h5 className="card-title">{cat.name}</h5>
-                <Link to={`/${cat.id}`} className="btn btn-primary me-2">
-                  Details
-                </Link>
-                <button
-                  onClick={() => {
-                    dispatch(
-                      updateProductQuantity({ product: cat, quantity: 1 })
-                    );
-                    dispatch(addToCart({ product: cat, quantity: 1 }));
-                  }}
-                  disabled={cat.quantity === 0}
-                  className="btn btn-primary"
-                >
-                  {cat.quantity > 0 ? (
-                    <span>TAKE ME HOME</span>
-                  ) : (
-                    <span>TAKEN</span>
-                  )}
-                </button>
-                <button
-                  onClick={() => addToWishlistHandler(cat)}
-                  className="btn btn-outline-danger"
-                >
-                  <i className="far fa-heart"></i>
-                </button>
-              </div>
-            </div>
-          </div>
-        ))}
+   return (
+      <div className="container text-center">
+         <div className="row">
+            {currentCats.map((cat) => (
+               <div key={cat.id} className="col-lg-4 col-md-6 col-sm-12 mb-4">
+                  <div className="card h-100">
+                     <div className="ratio ratio-4x3">
+                        <img className="card-img-top img-fluid" src={cat.images[0]} alt={cat.name} />
+                     </div>
+                     <div className="card-body">
+                        <h5 className="card-title">{cat.name}</h5>
+                        <Link to={`/${cat.id}`} className="btn btn-primary me-2">
+                           Details
+                        </Link>
+                        <button
+                           onClick={() => {
+                              dispatch(updateProductQuantity({ product: cat, quantity: 1 }));
+                              dispatch(addToCart({ product: cat, quantity: 1 }));
+                           }}
+                           disabled={cat.quantity === 0}
+                           className="btn btn-primary">
+                           {cat.quantity > 0 ? <span>TAKE ME HOME</span> : <span>TAKEN</span>}
+                        </button>
+                        <button onClick={() => addToWishlistHandler(cat)} className="btn btn-outline-danger ms-5">
+                           <i className="far fa-heart"></i>
+                        </button>
+                     </div>
+                  </div>
+               </div>
+            ))}
+         </div>
+         <nav className="Page navigation mb-5 d-flex justify-content-center align-items-center">
+            <ul className="pagination my-4" style={{ gap: "10px" }}>
+               <li className={`page-item ${currentPage === 1 ? "disabled" : ""}`}>
+                  <button className="page-link" onClick={handlePreviousPage}>
+                     Previous
+                  </button>
+               </li>
+               {Array.from({ length: totalPages }, (_, index) => index + 1).map((page) => (
+                  <li className={`page-item ${page === currentPage ? "active" : ""}`} key={page}>
+                     <button className="page-link" onClick={() => setCurrentPage(page)}>
+                        {page}
+                     </button>
+                  </li>
+               ))}
+               <li className={`page-item ${currentPage === totalPages ? "disabled" : ""}`}>
+                  <button className="page-link" onClick={handleNextPage}>
+                     Next
+                  </button>
+               </li>
+            </ul>
+         </nav>
       </div>
-      <button 
-        onClick={hanelPreviousPage}
-        disabled={currentPage<=0}
-      >
-        previous
-      </button>
-      <span>{currentPage}</span>
-      <button 
-        onClick={handelNextPage}
-        disabled={currentPage>=totalPages}
-      >
-        next
-      </button>
-     
-    </div>
-  );
+   );
 };
 
 export default AllCats;
